da9b368c651b161026fe737461c5cca24e7e02db
seyedsalar <39945203+seyedsalar@users.noreply.github.com>
2019-02-06 05:59:23 +0330

    Create Election.php

---
+++ login/Election.php
@@ -0,0 +1,67 @@
+
+
+
+
+<html>
+<head>
+  <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+
+</head>
+<body>
+
+
+<div class="form-wrapper">
+	<h2>Jesus & Burritos</h2>
+	<p>Do you think Jesus could microwave a burrito, so hot, the he, himself, could not eat it?</p>
+	<form action="index.php" method="post">
+		<label for="choice-1">
+			<input type="radio" id="choice-1" name="choice" value="1" />
+			<div>
+				No. The Lord can do anything.
+				<span>Are you sure?</span>
+			</div>
+		</label>
+		
+		<label for="choice-2">
+			<input type="radio" id="choice-2" name="choice" value="2" />
+			<div>
+				Jesus didn't have microwaves.
+				<span>Good point.</span>
+			</div>
+		</label>
+		
+		<label for="choice-3">
+			<input type="radio" id="choice-3" name="choice" value="3" />
+			<div>
+				He had to wait at least 3 minutes.
+				<span>Prove it.</span>
+			</div>
+		</label>
+		
+		<label for="choice-4">
+			<input type="radio" id="choice-4" name="choice" value="4" />
+			<div>
+				Depends on what kind of drink he has.
+				<span>No it doesn't.</span>
+			</div>
+		</label>
+		
+		<label for="choice-5">
+			<input type="radio" id="choice-5" name="choice" value="5" />
+			<div>
+				Yes. Why was he so special?
+				<span>Because he was a Carpenter.</span>
+			</div>
+		</label>
+		<button type="submit">Submit</button>
+	</form>
+</div>
+
+</body>
+</html>
+
+
+
+
