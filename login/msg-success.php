d6cc15675d2676d07e015e9c170f4566f40108eb
seyedsalar <39945203+seyedsalar@users.noreply.github.com>
2019-02-06 06:11:02 +0330

    Create msg-success.php

---
+++ login/msg-success.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="base.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="tac">
+  <br>
+  <img src="image/success.png">
+  <br>
+  <br>
+  <div><?=$message?></div>
+</div>
+</body>
+</html>
