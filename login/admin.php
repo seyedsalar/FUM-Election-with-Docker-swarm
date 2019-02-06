569d415c886d61ec7777069caa8d01ddaae14102
seyedsalar <39945203+seyedsalar@users.noreply.github.com>
2019-02-06 05:57:35 +0330

    Create admin.php

---
+++ login/admin.php
@@ -0,0 +1,46 @@
+
+<html>
+<head>
+  <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet"/>
+
+    
+</head>
+<body>
+
+<div class="container">
+        <div id="login-row" class="row justify-content-center align-items-center">
+            <div id="login-column" class="col-md-6">
+                <div class="box">
+                    <div class="shape1"></div>
+                    <div class="shape2"></div>
+                    <div class="shape3"></div>
+                    <div class="shape4"></div>
+                    <div class="shape5"></div>
+                    <div class="shape6"></div>
+                    <div class="shape7"></div>
+                    <div class="float">
+                        <form class="col-md-offset-2 form" action="Election-check.php" method="post">
+                            <div class="form-group">
+                                <label for="username" class="text-white">نام انتخابات را وارد کنید</label><br>
+                                <input type="text" name="name" id="name" class="form-control">
+                            </div>
+                          
+                            <div class="form-group">
+                                <input type="submit" name="submit" class="btn btn-success btn-md" value="افزودن">
+				<a href="Election_domain.php" class="btn btn-warning btn-md" >افزودن زیر مجموعه</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</body>
+</html>
