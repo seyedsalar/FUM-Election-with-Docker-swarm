aabf2c0f43fde3bcbe5a99dbc525af1b2824af5f
seyedsalar <39945203+seyedsalar@users.noreply.github.com>
2019-02-06 05:36:49 +0330

    Create main.php

---
+++ fraudDetection/main.php
@@ -0,0 +1,15 @@
+<?
+
+
+date_default_timezone_set('Asia/Tehran');
+
+global $config;
+require_once('config.php');
+//require_once('common.php');
+require_once('db.php');
+//require_once ('access.php');
+//require_once('locale-' . $config['lang'] . '.php');
+//require __DIR__ . '/vendor/autoload.php';
+//require_once("rdb/rdb.php");
+
+?>
