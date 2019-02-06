3a683f877d11d2d72d77c4004fe24120029a5fa1
seyedsalar <39945203+seyedsalar@users.noreply.github.com>
2019-02-06 06:08:54 +0330

    Create msg-fail.php

---
+++ login/msg-fail.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+
+  <link rel="stylesheet" href="css/style.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
+<div class="tac">
+
+
+
+<div class="col-md-12">
+  <div class="row">
+              <a href="register.php"  class="btn btn-xlarge" /><i class="fa fa-pencil-square-o  fa-5x" ></i></a>
+              <a href="index.php"  class="btn btn-xlarge" /><i class="fa fa-chevron-up fa-5x" ></i></a>
+    </div>
+</div>
+</br>
+<div class=" row col-md-12">
+<h3 style="color:#171414"><?=$message?></h3>
+</div>
+
+
+</div>
+</div>
+</body>
+</html>
