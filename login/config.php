df3873559bcaabe30b90e2b3bc4c9b54faa42490
seyedsalar <39945203+seyedsalar@users.noreply.github.com>
2019-02-06 05:58:45 +0330

    Create config.php

---
+++ login/config.php
@@ -0,0 +1,10 @@
+<?
+global $config;
+$config['db']['host'] = 'first_mysql_1';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = 'password';
+$config['db']['name'] = 'cc_project_database';
+
+$config['lang'] = 'fa';
+
+$config['salt'] = 'suya9s8ydaiu987vqo28bv9q87B87VPq7E98QVB';
